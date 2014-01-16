3efadb62a89501fde9698fe28631b028e9dea29a
Chris Hawthorn <komodo1138@gmail.com>
2014-01-16 14:42:51 -0500

    Initial Commit

---
+++ Platform_Scroller Tutorial/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player;
+    private GameCamera cam;
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<GameCamera>();
+        SpawnPlayer();
+	}
+
+    private void SpawnPlayer() 
+    {
+        cam.SetTarget((Instantiate(player, new Vector3(0,3,0), Quaternion.identity) as GameObject).transform);
+    }
+
+
+
+}
